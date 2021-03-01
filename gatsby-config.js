402b31af7d29e6aa798ffc37f2881beff43d7a96
davydka <davydka@gmail.com>
2021-03-01 13:57:18 -0800

    chore: setup gatsby directories and dependencies

---
+++ gatsby-config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  siteMetadata: {
+    name: `Hello Mock World!`,
+    tagline: `Gatsby + SASS + Typescript + MSW = 💪`
+  },
+  plugins: [
+    `gatsby-plugin-sass`,
+    `gatsby-plugin-typescript`,
+    `gatsby-plugin-tslint`,
+  ],
+}
